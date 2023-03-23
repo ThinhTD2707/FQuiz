22fb8c703c66691747c5fb4bdaba3a058f6909f1
thinh <thinhtdse150375@fpt.edu.vn>
2023-03-23 22:25:59 +0700

    change firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,26 +4,26 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB7SDQ51qgHzta1Xt4qAVu7nFGHGT6ByPI",
-//   authDomain: "swd-project-f0508.firebaseapp.com",
-//   projectId: "swd-project-f0508",
-//   storageBucket: "swd-project-f0508.appspot.com",
-//   messagingSenderId: "534900378275",
-//   appId: "1:534900378275:web:ed30c7ab9bb3da0d6ccd16",
-//   measurementId: "G-10PR1DZRFE"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyB7SDQ51qgHzta1Xt4qAVu7nFGHGT6ByPI",
+  authDomain: "swd-project-f0508.firebaseapp.com",
+  projectId: "swd-project-f0508",
+  storageBucket: "swd-project-f0508.appspot.com",
+  messagingSenderId: "534900378275",
+  appId: "1:534900378275:web:ed30c7ab9bb3da0d6ccd16",
+  measurementId: "G-10PR1DZRFE"
+};
 
 // FIREBASE BACKUP
-const firebaseConfig = { 
-  apiKey: "AIzaSyAv4jRmc13b80JVVRSL2KWjWdeAPV6-2hk",
-  authDomain: "quizz-app-93e02.firebaseapp.com",
-  projectId: "quizz-app-93e02",
-  storageBucket: "quizz-app-93e02.appspot.com",
-  messagingSenderId: "707606665638",
-  // appId: "1:904516436073:web:a348ba6fac45f5076c62f8",
-  // measurementId: "G-HMF1YHRT6P"
-};
+// const firebaseConfig = { 
+//   apiKey: "AIzaSyAv4jRmc13b80JVVRSL2KWjWdeAPV6-2hk",
+//   authDomain: "quizz-app-93e02.firebaseapp.com",
+//   projectId: "quizz-app-93e02",
+//   storageBucket: "quizz-app-93e02.appspot.com",
+//   messagingSenderId: "707606665638",
+//   // appId: "1:904516436073:web:a348ba6fac45f5076c62f8",
+//   // measurementId: "G-HMF1YHRT6P"
+// };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
